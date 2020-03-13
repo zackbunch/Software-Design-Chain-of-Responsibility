d82b5894150192e2ee04e6ed86bd8b6a5232a828
unknown <zackbunch96@gmail.com>
2020-03-12 20:25:44 -0400

    new

--- prob2.py
+++ prob2.py
@@ -1,10 +1,3 @@
-# (4 points) Implement a Chain of Responsibility-based program that does the following:
-# Accepts the name of a file.
-# Does one of the following actions:
-# Identifies the file's type as known, based on its extension, then
-# uses the associated utility to open the file,
-# displays an error message if the open fails
-# Displays an error message, noting that the file type isn't recognized.
 
 import os
 import fnmatch
